a756ef1b1fbde253f404f18995bb41483a879f45
cwsantos <chrissantos.149@outlook.com>
2018-10-26 12:39:30 -0600

    additions to spider file

---
+++ Assignment_3/Assignment_3/spiders/santos_christopher.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SantosChristopherSpider(scrapy.Spider):
+    name = 'santos_christopher'
+    start_urls = [
+        'http://http://www.cs.utep.edu/makbar/A3/A2.html'
+    ]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        output = "santos_christopher.txt"
+        with open(output, 'wb') as o:
+            o.write(response.body)
+        self.log('Saved file to %s' % output)
