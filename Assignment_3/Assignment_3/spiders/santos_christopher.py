d0afa36ce37f8dc818a5fefc61b7f52da56eed13
cwsantos <chrissantos.149@outlook.com>
2018-10-30 00:03:26 -0600

    spider file now creates txt files to parse through easier

--- Assignment_3/Assignment_3/spiders/santos_christopher.py
+++ Assignment_3/Assignment_3/spiders/santos_christopher.py
@@ -9,27 +9,45 @@ Last Modified: Oct. 29th, 2018
 
 class SantosChristopherSpider(scrapy.Spider):
     name = 'santos_christopher'
-    start_urls = {
-        # Holds the base and accomponing urls, and if visited
-        'http://www.cs.utep.edu/makbar/A3/A2.html':True
-    }
+    BASEURL = 'http://www.cs.utep.edu/makbar/A3'
+    visited = [
+        'http://www.cs.utep.edu/makbar/A3/A2.html'
+    ]
+    words = {}
 
     """
     Scrapy will start this method initial and send requests
     to allow crawling, respecting the rules of the robots.txt
     """
     def start_requests(self):
-        for url in self.start_urls:
+        for url in self.visited:
             yield scrapy.Request(url=url, callback=self.parse)
 
     """
     Parses through the output response and identifies the links
-    to other URL pages.
+    to other URL pages, and crawls those links
     """
     def parse(self, response):
-        self.log('Crawling stopped (finished)')
-        output = "santos_christopher.txt"
-        for link in response.xpath('//a/@href'):
-            self.log('\n\tLink found: %s' % link)
-            self.start_urls[link] = False
-        self.log('Stopping spider (finished)')
+        output = 'santos_christopher.txt'
+        for link in response.xpath('//a/@href').extract():
+            if link in self.visited:
+                continue
+            else:
+                self.log('>> Link found: %s' % link)
+                self.visited.append(link)
+                self.parseBody(link, response.body)
+                link = link[:0] + link[1:]
+                new_link = self.BASEURL + link
+                yield scrapy.Request(url=new_link, callback=self.parse)
+
+    """
+    Creates a text file to parse through and count the number of
+    words in the webpage, as well as count how many times they
+    appear
+    """
+    def parseBody(self, link, body):
+        link = link.replace('/', '-')
+        link = link.replace('.html', '')
+        link = link[2:4] + link[4:len(link)]
+        with open(('%s_body.txt' % link), 'wb') as o:
+            o.write(body)
